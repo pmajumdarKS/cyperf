acfbf7666ca280c5e2a69d2bddc2dc673fdcfb23
Partha Majumdar <partha.majumdar@keysight.com>
2024-08-24 01:56:48 +0000

    Added some code to collect agent information from the controller.

--- cyperf/utils/test.py
+++ cyperf/utils/test.py
@@ -28,6 +28,9 @@ class TestRunner(object):
         self.configuration.access_token = self.__authorize__()
         self.configuration.verify_ssl   = False
         self.apiClient     = cyperf.ApiClient(self.configuration)
+        self.agents        = {}
+
+        self.RefreshAgents()
 
     def __authorize__(self):
         session = requests.Session()
@@ -49,6 +52,17 @@ class TestRunner(object):
     def Authorize(self):
         self.configuration.access_token = self.__authorize__()
 
+    def RefreshAgents(self):
+        apiInstance = cyperf.AgentsApi(self.apiClient)
+        try:
+            response = apiInstance.api_v2_agents_get() ## This is an object instead of a json
+            agents   = response.actual_instance
+            for agent in agents:
+                self.agents [agent.ip] = agent
+            pprint (self.agents)
+        except cyperf.exceptions.ApiException as e:
+            pprint (e)
+
     def LoadConfigs(self, configFiles):
         apiInstance = cyperf.ConfigurationsApi(self.apiClient)
         results     = []
@@ -59,19 +73,83 @@ class TestRunner(object):
             if 'SUCCESS' == response.state:
                 results += response.result
 
-        return [cyperf.AppsecConfig.from_dict(e['configData']) for e in results]
+        #pprint(results)
+        # Why isn't configUrl a part of AppsecConfig?
+        configs = {}
+        for e in results:
+            configs [e['id']] = (e['configUrl'], cyperf.AppsecConfig.from_dict(e['configData']))
+        return configs
 
     def DeleteConfig(self, configs):
         apiInstance = cyperf.ConfigurationsApi(self.apiClient)
-        for config in configs:
+        for configUrl, config in configs.values():
             apiInstance.api_v2_configs_config_id_delete (config.id)
 
+    def LaunchSessions(self, configs):
+        apiInstance = cyperf.SessionsApi(self.apiClient)
+        sessions    = []
+        for configUrl, config in configs.values():
+            session = cyperf.Session()
+            session.config     = config
+            session.config_url = configUrl
+            sessions.append (session)
+        try:
+            sessions = apiInstance.api_v2_sessions_post(session=sessions)
+            for session in sessions:
+                session.config = configs[session.config_url][1]
+            return sessions
+        except cyperf.exceptions.ApiException as e:
+            pprint (e)
+            return []
+
+    def CloseSessions(self, sessions):
+        apiInstance = cyperf.SessionsApi(self.apiClient)
+        for session in sessions:
+            try:
+                apiInstance.api_v2_sessions_session_id_delete (session.id)
+            except cyperf.exceptions.ApiException as e:
+                pprint (e)
+
+    def __updateAgents__(self, session, agentMapping):
+        config      = session.config.config
+        netProfiles = config.network_profiles
+        for netProfile in netProfiles:
+            ipNets = netProfile.ip_network_segment
+            for ipNet in ipNets:
+                if ipNet.name in agentMapping:
+                    pprint (f"Found an agent for {ipNet.name} with ip {agentMapping[ipNet.name]}")
+                    if not ipNet.agent_assignments:
+                        ipNet.agent_assignments = cyperf.AgentAssignments()
+                    agents = agentMapping[ipNet.name]
+                    for agentName in agents:
+                        agent = self.agents[agentName]
+                        interfaces = [iFace.name for iFace in agent.interfaces]
+                        agentDetails = cyperf.AgentAssignmentDetails(agent_id = agent.id, id = agent.id, interfaces = interfaces)
+                        ipNet.agent_assignments.by_id.append(agentDetails)
+        apiInstance = cyperf.SessionsApi(self.apiClient)
+        #try:
+        #    session.config = apiInstance.api_v2_sessions_session_id_config_put(session.id, session.config)
+        #    pprint (session)
+        #except cyperf.exceptions.ApiException as e:
+        #    pprint (e)
+
+    def UpdateAgents(self, sessions, agentMapping):
+        for session in sessions:
+            self.__updateAgents__(session, agentMapping)
+
     def Run(self, config):
-        pprint (f"Loading configuration {config.file} ...")
-        configs = self.LoadConfigs ([config.file])
-        pprint (f"Loaded configuration {config.file} ...")
+        width = 200
+        pprint (f"Loading configuration {config.file} ...", width=width)
+        configs  = self.LoadConfigs ([config.file])
+        pprint (f"Launching session for configuration {config.file} ...", width=width)
+        sessions = self.LaunchSessions (configs)
+        pprint (f"Updating agents as {config.agents} ...", width=width)
+        self.UpdateAgents (sessions, config.agents)
+        pprint (f"Started test for configuration {config.file} ... session id: {sessions[0].index}", width=width)
         time.sleep(10)
-        pprint (f"Deleting configuration {config.file} ...")
+        pprint (f"Closing session for configuration {config.file} ...", width=width)
+        self.CloseSessions (sessions)
+        pprint (f"Deleting configuration {config.file} ...", width=width)
         self.DeleteConfig (configs)
-        pprint (f"Deleted configuration {config.file} ...")
+        pprint (f"Deleted configuration {config.file} ...", width=width)
 
