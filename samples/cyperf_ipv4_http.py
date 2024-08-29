764a4cdb71bf036b1679fe256e6596d0166214f0
Partha Majumdar <partha.majumdar@keysight.com>
2024-08-29 20:36:30 +0000

    Updating IP addresses. It is not too important to be submitted. Still submitting for making my life simpler.

--- samples/cyperf_ipv4_http.py
+++ samples/cyperf_ipv4_http.py
@@ -1,10 +1,10 @@
 from cyperf.utils.test import TestRunner, TestConfig
 
 tests = [
-    TestConfig ('samples/configs/Msoft_tput_smartNIC_passthru.zip', {'IP Network Segment 1': ['10.36.75.235'], 'IP Network Segment 2': ['10.36.75.246']}, 'Throughput', 12500000000, None)
+    TestConfig ('samples/configs/Msoft_tput_smartNIC_passthru.zip', {'IP Network Segment 1': ['10.36.74.185'], 'IP Network Segment 2': ['10.36.74.196']}, 'Throughput', 12500000000, None)
 ]
 
-runner = TestRunner ('10.36.75.241', 'admin', 'CyPerf&Keysight#1')
+runner = TestRunner ('10.36.75.169', 'admin', 'CyPerf&Keysight#1')
 
 for config in tests:
     runner.Run (config)
